acaa86d2d0562e14eccdf97e6e3ecfc69e23e643
kevinhu01 <kevinhu01@yahoo.com>
2026-04-05 17:50:28 -0400

    Update run_fc.sh parameters

--- run_fc.sh
+++ run_fc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=featureCounts
 #SBATCH --output=featureCounts.log
+#SBATCH --partition=angsd_class
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=40G
 #SBATCH --time=6:00:00
 
-featureCounts -T 6 -s 2 -p -t exon -g gene_id --countReadPairs -a referenceGenome/hg38.knownGene.gtf -o ./counts_data.txt LOAD/final_alignments/*.bam WT/final_alignments/*.bam
+featureCounts -T 8 -s 2 -p -t transcript -g gene_id --countReadPairs -a referenceGenome/hg38.knownGene.gtf -o ./counts_matrix.txt LOAD/final_alignments/*.bam WT/final_alignments/*.bam
